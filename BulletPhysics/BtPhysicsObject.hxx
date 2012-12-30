d53c759301fd66e9c43d3b0864a426e514a90d26
mayingzhen <436406@163.com>
2012-12-30 04:52:16 +0000

    Bullet物理接入

--- BulletPhysics/BtPhysicsObject.hxx
+++ BulletPhysics/BtPhysicsObject.hxx
@@ -1,4 +1,5 @@
 #include "BulletPhysics/BtPhysicsObject.h"
+#include "BulletPhysics/BulletUtil.h"
 
 namespace ma
 {
@@ -19,11 +20,21 @@ namespace ma
 
 	void BtPhysicsObject::SetTransformWS(const maNodeTransform& tsfWS)
 	{
+		if (m_pRigidBody == NULL)
+			return;
 
+		m_pRigidBody->setWorldTransform( ToBulletUnit(tsfWS) );
 	}
 
 	maNodeTransform BtPhysicsObject::GetTransformWS() const
 	{
+		if (m_pRigidBody == NULL)
+		{
+			maNodeTransform tsfWS;
+			maTransformSetIdentity(&tsfWS);
+			return tsfWS;
+		}
 
+		return ToMaUnit( m_pRigidBody->getWorldTransform() );
 	}
 }
