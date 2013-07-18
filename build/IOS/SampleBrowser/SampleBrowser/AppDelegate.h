35ccbc9d78f1bc15bbe3f9ea4398ac9bfe6cbda0
mayingzhen <436406@163.com>
2013-07-18 03:30:23 +0000

    xcode project

---
+++ build/IOS/SampleBrowser/SampleBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleBrowser
+//
+//  Created by mayingzhen on 7/16/13.
+//  Copyright (c) 2013 nd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
