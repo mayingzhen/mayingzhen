33ba786373076c4e72ae13ead8bd25eb53a61b92
mayingzhen <436406@163.com>
2018-08-17 23:56:36 +0800

    RenderThread 重构

--- code/Engine/RenderSystem/RenderThread.h
+++ code/Engine/RenderSystem/RenderThread.h
@@ -10,42 +10,11 @@ namespace ma
 	class Uniform;
 	class RenderPass;
 	
-	enum ERenderCommand
-	{
-		eRC_Unknown = 0,
 
-		eRC_Init,
-		eRC_Reset,
-		eRC_ShutDown,
 
-		eRC_TechniqueStreamComplete,
-		eRC_TexStreamComplete,
-		eRC_ShaderStreamComplete,
-		eRC_VertexDeclaComplete,
-		eRC_HBStreamComplete,
-		eRC_RenderPassStreamComplete,
-
-		eRC_BeginRender,
-		eRC_EndRender,
-		eRC_Render,
-		eRC_CreateShader,
-		eRC_CreateTexture,
-
-		eRC_SetUniformValue,
-		eRC_SetSampler,
-		eRC_SetStorageBuffer,
-		
-		eRC_SetPoolId,
-
-		eRC_BeginProfile,
-		eRC_EndProfile,
-	};
-
-	struct RenderCommad /*: public Referenced*/
+	struct CommadInfo 
 	{
 		std::function<void()> m_funtion;
-
-		virtual void Do() { m_funtion(); }
 	};
 
 
@@ -62,31 +31,6 @@ namespace ma
 		void	WaitFlushCond();
 		void	WaitFlushFinishedCond();
 		bool	CheckFlushCond();
-
-		// Command
-		void	AddCommand(ERenderCommand eRC);
-		void	AddDWORD(uint32_t nVal);
-		void	AddInt(int nVal);
-		void	AddBool(bool bVal);
-		void	AddFloat(float fVal);
-		void	AddVec2(const Vector2& cVal);
-		void	AddVec3(const Vector3& cVal);
-		void	AddVec4(const Vector4& cVal);
-		void	AddColor(const ColourValue& cVal);
-		void	AddMatrix4(const Matrix4& cVal);
-		void	AddPointer(const void *pVal);
-		uint8_t *	AddData(const void *pData, int nLen);
-
-		template<class T>
-		void	ReadData(int &nIndex,T& data, uint32_t& nSize);
-		
-		template<class T>
-		T		ReadDataPtr(int &nIndex,int nLen);
-
-		void	ReadString(int &nIndex,char* pStr, uint32_t nInLen);
-
-		template<class T> 
-		T		ReadCommand(int& nIndex);
 
 		void	ThreadLoop();
 		void	Start();
@@ -126,8 +70,10 @@ namespace ma
 		void	RC_BeginProfile(const char* pszLale);
 		void	RC_EndProfile();
 
-		void    RC_AddRenderCommad(std::function<void()> fun);
-		//void    RC_AddRenderCommad(RenderCommad* pCommad);
+		bool    RC_AddRenderCommad(const std::function<void()>& fun);
+
+	private:
+		uint8_t*	AddData(const void *pData, int nLen);
 
 	private:
 		bool			m_bMultithread;
@@ -140,128 +86,23 @@ namespace ma
 		volatile int	m_nFlush;
 
 		std::thread::id	m_nMainThread;
-		TArray<uint8_t>	m_Commands[2]; // m_nCurThreadFill shows which commands are filled by main thread
-
+		
 		std::thread		m_thread;
 
 		bool			m_bExit = false;
 
-		std::vector< RenderCommad > m_vecCommand[2];
+		std::vector< CommadInfo > m_vecCommand[2];
+
+		TArray<uint8_t>	m_CommandData[2]; // m_nCurThreadFill shows which commands are filled by main thread
 	};
-    
-    
-    inline void RenderThread::AddCommand(ERenderCommand eRC)
-	{
-		m_Commands[m_nCurThreadFill].AddElem(eRC);
-	}
-    
-	inline void RenderThread::AddDWORD(uint32_t nVal)
-	{
-		uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(uint32_t));
-        memcpy(pDest,&nVal,sizeof(uint32_t));
-	}
-   
-    
-	inline void RenderThread::AddInt(int nVal)
-	{
-		*(int *)(m_Commands[m_nCurThreadFill].Grow(sizeof(int))) = nVal;
-	}
-    
-	inline void RenderThread::AddBool(bool bVal)
-	{
-		*(bool *)(m_Commands[m_nCurThreadFill].Grow(sizeof(bool))) = bVal;
-	}
-    
-    
-	inline void RenderThread::AddFloat(float fVal)
-	{
-        uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(float));
-        memcpy(pDest,&fVal,sizeof(float)); // ARM 下不能使用指针强转赋值
-	}
-
-	inline void RenderThread::AddVec2(const Vector2& vVal)
-	{
-		uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(Vector2));
-		memcpy(pDest,&vVal,sizeof(Vector2));
-	}
-    
-	inline void RenderThread::AddVec3(const Vector3& vVal)
-	{
-        uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(Vector3));
-        memcpy(pDest,&vVal,sizeof(Vector3));
-	}
-
-	inline void RenderThread::AddVec4(const Vector4& cVal)
-	{
-		uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(Vector4));
-		memcpy(pDest,&cVal,sizeof(Vector4));
-	}
-    
-	inline void RenderThread::AddColor(const ColourValue& cVal)
-	{
-		uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(ColourValue));
-        memcpy(pDest,&cVal,sizeof(ColourValue));  
-	}
-
-	inline void RenderThread::AddMatrix4(const Matrix4& cVal)
-	{
-		uint8_t* pDest = m_Commands[m_nCurThreadFill].Grow(sizeof(Matrix4));
-		memcpy(pDest,&cVal,sizeof(Matrix4));  
-	}
-    
-	inline void RenderThread::AddPointer(const void *pVal)
-	{
-		*(const void **)(m_Commands[m_nCurThreadFill].Grow(sizeof(void *))) = pVal;
-	}
     
 	inline uint8_t * RenderThread::AddData(const void *pData, int nLen)
 	{
-		AddDWORD(nLen);
-		uint8_t *pDst = m_Commands[m_nCurThreadFill].Grow(nLen);
+		uint8_t *pDst = m_CommandData[m_nCurThreadFill].Grow(nLen);
 		memcpy(pDst, pData, nLen);
 		return pDst;
 	}
 
-	template<class T>
-	T RenderThread::ReadDataPtr(int &nIndex,int nLen)
-	{
-		uint32_t nReadLen = ReadCommand<uint32_t>(nIndex);
-		ASSERT(nLen == nReadLen);
-		uint8_t* pSrc = &m_Commands[m_nCurThreadProcess][nIndex]; 
-		nIndex += nReadLen;
-		return (T)pSrc;
-	}
-    
-	template<class T>
-	inline void	RenderThread::ReadData(int &nIndex,T& data,uint32_t& nSize)
-	{
-		uint32_t nLen = ReadCommand<uint32_t>(nIndex);
-		//ASSERT(nLen == sizeof(T));
-		uint8_t* pSrc = &m_Commands[m_nCurThreadProcess][nIndex]; 
-		//memcpy(&data,pSrc,nLen);
-		data = (T)pSrc;
-		nSize = nLen;
-		nIndex += nLen;
-	}
-
-	inline void	RenderThread::ReadString(int &nIndex,char* pStr,uint32_t nInLen)
-	{
-		uint32_t nLen = ReadCommand<uint32_t>(nIndex);
-		ASSERT(nLen <= nInLen);
-		uint8_t* pSrc = &m_Commands[m_nCurThreadProcess][nIndex]; 
-		memcpy(pStr,pSrc,nLen);
-		nIndex += nLen;
-	}
-    
-	template<class T>
-	inline T RenderThread::ReadCommand(int& nIndex)
-	{
-		T Res = *(T*)&m_Commands[m_nCurThreadProcess][nIndex];
-		nIndex += sizeof(T);
-		return Res;
-	}
-    
-    
 	inline void RenderThread::SignalFlushFinishedCond()
 	{
 		m_nFlush = 0;
--- code/Engine/RenderSystem/RenderThread.hxx
+++ code/Engine/RenderSystem/RenderThread.hxx
@@ -63,91 +63,70 @@ namespace ma
 
 	void RenderThread::RC_Init(void* wndhandle)
 	{
-		if (IsRenderThread())
-		{
-			return GetRenderSystem()->RT_Init(wndhandle);
-		}
+		RC_AddRenderCommad([wndhandle]() {
+			GetRenderSystem()->RT_Init(wndhandle);
+		});
 
-		AddCommand(eRC_Init);
-		AddPointer(wndhandle);
+		if (IsRenderThread())
+			return;
 
 		FlushAndWait();
 	}
 
 	void RenderThread::RC_Reset(uint32_t nWidth,uint32_t nHeight)
 	{
-		if (IsRenderThread())
-		{
-			return GetRenderSystem()->RT_Reset(nWidth,nHeight);
-		}
+		RC_AddRenderCommad([nWidth,nHeight]() {
+			GetRenderSystem()->RT_Reset(nWidth, nHeight);
+		});
 
-		AddCommand(eRC_Reset);
-		AddDWORD(nWidth);
-		AddDWORD(nHeight);
+		if (IsRenderThread())
+			return;
 
 		FlushAndWait();
 	}
 
 	void RenderThread::RC_ShutDown()
 	{
-		if (IsRenderThread())
-		{
-			return GetRenderSystem()->RT_ShutDown();
-		}
+		RC_AddRenderCommad([]() {
+			GetRenderSystem()->RT_ShutDown();
+		});
 
-		AddCommand(eRC_ShutDown);
+		if (IsRenderThread())
+			return;
 
 		FlushAndWait();
 	}
 
 	void RenderThread::RC_BeginRender()
 	{
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([]() {
 			GetRenderSystem()->RT_BeginRender();
-			return;
-		}
-
-		AddCommand(eRC_BeginRender);
+		});
 	}
 
 	void RenderThread::RC_EndRender()
 	{
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([]() {
 			GetRenderSystem()->RT_EndRender();
-			return;
-		}
-
-		AddCommand(eRC_EndRender);
+		});
 	}
 
 	void RenderThread::RC_TechniqueStreamComplete(Technique* pTech)
 	{
 		ASSERT(pTech);
 
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([pTech]() {
 			pTech->RT_StreamComplete();
-			return;
-		}
-
-		AddCommand(eRC_TechniqueStreamComplete);
-		AddPointer(pTech);
+		});
 	}
 
 	void RenderThread::RC_TexStreamComplete(Texture* pTexture)
 	{
 		ASSERT(pTexture);
 
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([pTexture]() {
 			pTexture->RT_StreamComplete();
-			return;
-		}
-
-		AddCommand(eRC_TexStreamComplete);
-		AddPointer(pTexture);
+		});
 	}
 
 
@@ -155,82 +134,52 @@ namespace ma
 	{
 		ASSERT(pShader);
 
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([pShader]() {
 			pShader->RT_StreamComplete();
-			return;
-		}
-
-		AddCommand(eRC_ShaderStreamComplete);
-		AddPointer(pShader);
+		});
 	}
 
 	void RenderThread::RC_VertexDeclaComplete(VertexDeclaration* pDecl)
 	{
 		ASSERT(pDecl);
 
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([pDecl]() {
 			pDecl->RT_StreamComplete();
-			return;
-		}
-
-		AddCommand(eRC_VertexDeclaComplete);
-		AddPointer(pDecl);
+		});
 	}
 
 	void RenderThread::RC_HardwareBufferStreamComplete(HardwareBuffer* pHB)
 	{
 		ASSERT(pHB);
 
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([pHB]() {
 			pHB->RT_StreamComplete();
-			return;
-		}
-
-		AddCommand(eRC_HBStreamComplete);
-		AddPointer(pHB);
+		});
 	}
 
 	void RenderThread::RC_RenderPassStreamComplete(RenderPass* pRenderPass)
 	{
 		ASSERT(pRenderPass);
 
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([pRenderPass]() {
 			pRenderPass->Create();
-			return;
-		}
-
-		AddCommand(eRC_RenderPassStreamComplete);
-		AddPointer(pRenderPass);
+		});
 	}
 
 	void RenderThread::RC_Render()
 	{
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([]() {
 			GetRenderSystem()->RT_Render();
-			return;
-		}
+		});
 
-		AddCommand(eRC_Render);
+		if (IsRenderThread())
+			return;
 
 		FlushFrame();
 	}
 
 	void RenderThread::RC_CreateShader(ShaderProgram* pShader)
 	{
-// 		if (IsRenderThread())
-// 		{
-// 			pShader->RT_StreamComplete();
-// 			return;
-// 		}
-// 
-// 		AddCommand(eRC_CreateShader);
-// 		AddPointer(pShader);
-
 		RC_AddRenderCommad( [pShader]() {
 				pShader->RT_StreamComplete();
 		});
@@ -238,23 +187,6 @@ namespace ma
 
 	void RenderThread::RC_CreateTexture(Texture* pRenderTarget)
 	{
-// 		if (IsRenderThread())
-// 		{
-// 			if (pRenderTarget->GetType() == TEXTYPE_CUBE)
-// 			{
-// 				pRenderTarget->RT_CreateCubeTexture();
-// 			}
-// 			else
-// 			{
-// 				pRenderTarget->RT_CreateTexture();
-// 			}
-// 			
-// 			return;
-// 		}
-// 
-// 		AddCommand(eRC_CreateTexture);
-// 		AddPointer(pRenderTarget);
-
 		RC_AddRenderCommad( 
 			[pRenderTarget]() {
 			if (pRenderTarget->GetType() == TEXTYPE_CUBE)
@@ -270,20 +202,8 @@ namespace ma
 
 	void RenderThread::RC_SetUniformValue(Uniform* pUniform, const void* data, uint32_t nSize)
 	{
-		// job 线程也会调用到这边
-		if (IsRenderThread() || !IsMainThread())
-		{
-			ASSERT(nSize <= pUniform->GetSize());
-			pUniform->GetParent()->SetParameter(pUniform->GetOffset(), pUniform->GetSize(), data);
-			return;
-		}
-
-		AddCommand(eRC_SetUniformValue);
-		AddPointer(pUniform);
-		AddData(data, nSize);
-
 		uint8_t* pCopyData = (uint8_t*)data;
-		if (!IsRenderThread() && IsMainThread())
+		if (!IsRenderThread() && IsMainThread()) // job 线程也会调用到这边
 		{
 			pCopyData = AddData(data, nSize);
 		}
@@ -303,9 +223,9 @@ namespace ma
 			return;
 		}
 
-		AddCommand(eRC_SetSampler);
-		AddPointer(pUniform);
-		AddPointer(pSampler);
+		RC_AddRenderCommad([pUniform, pSampler]() {
+			pUniform->GetTechnique()->RT_SetSampler(pUniform, pSampler);
+		});
 	}
 
 	void RenderThread::RC_SetStorageBuffer(Uniform* pUniform, HardwareBuffer* pBuffer)
@@ -317,70 +237,53 @@ namespace ma
 			return;
 		}
 
-		AddCommand(eRC_SetStorageBuffer);
-		AddPointer(pUniform);
-		AddPointer(pBuffer);
+		RC_AddRenderCommad([pUniform, pBuffer]() {
+			pUniform->GetTechnique()->RT_SetStorageBuffer(pUniform, pBuffer);
+		});
 	}
 
 	void RenderThread::RC_SetPoolId(uint32_t poolId)
 	{
-		if (IsRenderThread())
-		{
+		RC_AddRenderCommad([poolId]() {
 			GetRenderSystem()->RT_SetPoolId(poolId);
-			return;
-		}
-
-		AddCommand(eRC_SetPoolId);
-		AddDWORD(poolId);
+		});
 	}
 
 	void RenderThread::RC_BeginProfile(const char* pszLale)
 	{
-// 		if (IsRenderThread())
-// 		{
-// 			GetRenderDevice()->BeginProfile(pszLale);
-// 			return;
-// 		}
-// 
-// 		AddCommand(eRC_BeginProfile);
-// 		AddData(pszLale,strlen(pszLale));
-
 		const char* pszParamLable = pszLale;
 		if (!IsRenderThread())
 		{
 			pszParamLable = (const char*)AddData(pszLale, strlen(pszLale));
 		}
 
-		RC_AddRenderCommad( [pszParamLable]() { GetRenderDevice()->BeginProfile(pszParamLable); } );
-
+		RC_AddRenderCommad( [pszParamLable]() { 
+			GetRenderDevice()->BeginProfile(pszParamLable); 
+		} );
 	}
 
 	void RenderThread::RC_EndProfile()
 	{
-// 		if (IsRenderThread())
-// 		{
-// 			GetRenderDevice()->EndProfile();
-// 			return;
-// 		}
-// 	
-// 		AddCommand(eRC_EndProfile);
-
-		RC_AddRenderCommad( []() { GetRenderDevice()->EndProfile(); } );
+		RC_AddRenderCommad( []() { 
+			GetRenderDevice()->EndProfile(); 
+		} );
 	}
 
-	void RenderThread::RC_AddRenderCommad(std::function<void()> fun)
+	bool RenderThread::RC_AddRenderCommad(const std::function<void()>& fun)
 	{
 		if (IsRenderThread())
 		{
 			fun();
-			return;
+			return false;
 		}
 		else
 		{
-			RenderCommad comd;
+			CommadInfo comd;
 			comd.m_funtion = fun;
 
 			m_vecCommand[m_nCurThreadFill].emplace_back(comd);
+
+			return true;
 		}
 	}
 
@@ -393,153 +296,11 @@ namespace ma
 
 		for (uint32_t i = 0; i < m_vecCommand[m_nCurThreadProcess].size(); ++i)
 		{
-			RenderCommad* pCommmad = m_vecCommand[m_nCurThreadProcess][i].get();
-			pCommmad->Do();
+			CommadInfo& pCommmad = m_vecCommand[m_nCurThreadProcess][i];
+			pCommmad.m_funtion();
 		}
 		m_vecCommand[m_nCurThreadProcess].clear();
 
-		int n = 0;
-		while (n < (int)m_Commands[m_nCurThreadProcess].Num())
-		{
-			uint8_t nC = m_Commands[m_nCurThreadProcess][n++];
-			switch (nC)
-			{
-			case eRC_Init:
-				{
-					void* wndhandle = ReadCommand<void*>(n);
-					GetRenderSystem()->RT_Init(wndhandle);
-				}
-				break;
-			case eRC_Reset:
-				{
-					uint32_t nWidth = ReadCommand<uint32_t>(n);
-					uint32_t nHeight = ReadCommand<uint32_t>(n);
-					GetRenderSystem()->RT_Reset(nWidth,nHeight);
-				}
-				break;
-			case eRC_ShutDown:
-				{
-					GetRenderSystem()->RT_ShutDown();
-				}
-				break;
-			case eRC_BeginRender:
-				{
-					GetRenderSystem()->RT_BeginRender();
-				}
-				break;
-			case eRC_EndRender:
-				{
-					GetRenderSystem()->RT_EndRender();
-				}
-				break;
-			case eRC_Render:
-				{
-					GetRenderSystem()->RT_Render();
-				}
-				break;
-			case  eRC_TechniqueStreamComplete:
-				{
-					Technique* pTech = ReadCommand<Technique*>(n);
-					pTech->RT_StreamComplete();
-				}
-				break;
-			case  eRC_TexStreamComplete:
-				{
-					Texture* pTexture = ReadCommand<Texture*>(n);
-					pTexture->RT_StreamComplete();
-				}
-				break;
-			case  eRC_ShaderStreamComplete:
-				{
-					ShaderProgram* pShader = ReadCommand<ShaderProgram*>(n);
-					pShader->RT_StreamComplete();
-				}
-				break;
-			case  eRC_VertexDeclaComplete:
-				{
-					VertexDeclaration* pDecl = ReadCommand<VertexDeclaration*>(n);
-					pDecl->RT_StreamComplete();
-				}
-				break;
-			case  eRC_HBStreamComplete:
-				{
-					HardwareBuffer* pHB = ReadCommand<HardwareBuffer*>(n);
-					pHB->RT_StreamComplete();
-				}
-				break;
-			case eRC_RenderPassStreamComplete:
-				{
-					RenderPass* pRenderPass = ReadCommand<RenderPass*>(n);
-					pRenderPass->Create();
-				}
-				break;
-			case  eRC_CreateShader:
-				{
-					ShaderProgram* pShader = ReadCommand<ShaderProgram*>(n);
-					pShader->RT_StreamComplete();
-				}
-				break;
-			case  eRC_CreateTexture:
-				{
-					Texture* pTarget = ReadCommand<Texture*>(n);
-					if (pTarget->GetType() == TEXTYPE_CUBE)
-					{
-						pTarget->RT_CreateCubeTexture();
-					}
-					else
-					{
-						pTarget->RT_CreateTexture();
-					}
-				}
-				break;
-			case eRC_SetUniformValue:
-				{
-					Uniform* pUniform = ReadCommand<Uniform*>(n);
-					void* data = NULL;
-					uint32_t nSize = 0;
-					ReadData(n, data, nSize);
-
-					ASSERT(nSize <= pUniform->GetSize());
-					pUniform->GetParent()->SetParameter(pUniform->GetOffset(), pUniform->GetSize(), data);
-				}
-				break;
-			case eRC_SetSampler:
-				{
-					Uniform* pUniform = ReadCommand<Uniform*>(n);
-					SamplerState* pSampler = ReadCommand<SamplerState*>(n);
-					pUniform->GetTechnique()->RT_SetSampler(pUniform, pSampler);
-				}
-				break;
-			case eRC_SetStorageBuffer:
-				{
-					Uniform* pUniform = ReadCommand<Uniform*>(n);
-					HardwareBuffer* pBuffer = ReadCommand<HardwareBuffer*>(n);
-					pUniform->GetTechnique()->RT_SetStorageBuffer(pUniform, pBuffer);
-				}
-				break;
-			case  eRC_SetPoolId:
-				{
-					uint32_t nId = ReadCommand<uint32_t>(n);
-					GetRenderSystem()->RT_SetPoolId(nId);
-				}
-				break;
-			case eRC_BeginProfile:
-				{
-					char pStr[256] = {0};
-					ReadString(n,pStr,256);
-					GetRenderDevice()->BeginProfile(pStr);
-				}
-				break;
-			case eRC_EndProfile:
-				{
-					GetRenderDevice()->EndProfile();
-				}	
-				break;
-			default:
-				ASSERT(false);
-				break;
-			}
-		}
 		SignalFlushFinishedCond();
 	}
 
@@ -560,7 +321,7 @@ namespace ma
 		WaitFlushFinishedCond();
 
 		m_nCurThreadProcess = nCurProcess;
-		m_Commands[m_nCurThreadFill].SetUse(0);
+		m_CommandData[m_nCurThreadFill].SetUse(0);
 	}
 
 
@@ -569,8 +330,8 @@ namespace ma
 	{
 		//FUNCTION_PROFILER_FAST( GetISystem(),PROFILE_RENDERER,g_bProfilerEnabled );
 
-		//if (!IsMultithreaded())
-		//	return;
+		if (!m_bMultithread)
+			return;
 
 		uint64_t nTime = GetTimer()->GetMillisceonds();
 		WaitFlushFinishedCond();
@@ -581,7 +342,7 @@ namespace ma
 		m_nCurThreadProcess = m_nCurThreadFill;
 		m_nCurThreadFill = (m_nCurThreadProcess+1) & 1;
 
-		m_Commands[m_nCurThreadFill].SetUse(0);
+		m_CommandData[m_nCurThreadFill].SetUse(0);
 		//gRenDev->m_fTimeProcessedRT[m_nCurThreadFill] = 0;
 		//gRenDev->m_fTimeWaitForMain[m_nCurThreadProcess] = 0;
 
